bf8169d210834b5b92a59defc5f1252b7f3b7c86
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2023-11-04 14:56:53 +0000

    (WIP) initial work to find column positions and display a marker

--- src/components/ConnectFourBoard.vue
+++ src/components/ConnectFourBoard.vue
@@ -1,6 +1,9 @@
 <template>
+    <div>
     <div id="board">
-        <div class="board-container d-none d-md-block">
+        <img v-show="isHovered" ref="marker" class="marker animate__animated animate__bounce animate__infinite" :src="redMarker" alt="red counter" />
+
+        <div ref=board class="board-container d-none d-md-block" @mouseover="startCounterCheck" @mouseleave="stopCounterCheck">
             <img class="back" :src="backBoardLarge" alt="back board" />
             <img class="front" :src="frontBoardLarge" alt="front board" />
         </div>
@@ -9,20 +12,80 @@
             <img class="front" :src="frontBoardSmall" alt="front board" />
         </div>
     </div>
+</div>
 </template>
 <script>
+import { ref, onMounted } from 'vue';
 import backBoardLarge from '../assets/images/board-layer-black-large.svg';
 import backBoardSmall from '../assets/images/board-layer-black-small.svg';
 import frontBoardLarge from '../assets/images/board-layer-white-large.svg';
 import frontBoardSmall from '../assets/images/board-layer-white-small.svg';
+import redMarker from '../assets/images/marker-red.svg';
 
 export default {
     setup() {
+        const board = ref(null);
+        const isHovered = ref(false);
+        const marker = ref(null);
+        const columns = 7;
+        // const rows = 6;
+        let columnPositions = [];
+        // const rowPositions = [];
+
+        onMounted(() => {
+            window.addEventListener("resize", getColumnPositions);
+            getColumnPositions(); 
+        });
+
+        const getColumnPositions = () => {
+            columnPositions = [];
+            for (let i = 0; i < columns; i++) {
+                columnPositions.push(board.value.offsetLeft + (i * (board.value.offsetWidth / columns)) + (board.value.offsetWidth / (columns * 2)) - (marker.value.width / 2) + 5 - i);
+            }
+
+            console.log(columnPositions)
+        }
+
+        const updateMarkerPosition = (event) => {
+            const x = event.clientX;
+            const y = event.clientY;
+            console.log(x, y);
+            const validPositions = columnPositions.filter((c) => c < x);
+            console.log(`c: ${columnPositions}, x: ${x}`)
+            console.log(validPositions);
+            const validPositionIndex = columnPositions.indexOf(validPositions[validPositions.length - 1]);
+            marker.value.style.left = `${columnPositions[validPositionIndex]}px`;
+            console.log('in column', validPositionIndex);
+        }
+
+        const startCounterCheck = () => {
+            marker.value.style.top = `${board.value.offsetTop - marker.value.height - 2}px`;
+            isHovered.value = true;
+
+            window.addEventListener('mousemove', updateMarkerPosition);
+        }
+
+        const stopCounterCheck = () => {
+            isHovered.value = false;
+            window.removeEventListener('mousemove', updateMarkerPosition);
+        }
+
         return {
+            // images
             backBoardLarge,
             backBoardSmall,
             frontBoardLarge,
             frontBoardSmall,
+            redMarker,
+
+            // refs
+            board,
+            isHovered,
+            marker,
+
+            // methods
+            startCounterCheck,
+            stopCounterCheck,
         }
     }
 }
@@ -52,6 +115,13 @@ export default {
                 left: 0;
                 z-index: 2;
             }
+        }
+        
+        .marker {
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform-origin: top left;
         }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap';
 import './assets/main.scss'
+import 'animate.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
