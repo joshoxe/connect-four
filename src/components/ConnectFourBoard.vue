cccd16b9b983ab9dd4cefd63664995e91596a36e
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2023-11-04 23:43:25 +0000

    reset all positioning logic to use CSS grid

--- src/components/ConnectFourBoard.vue
+++ src/components/ConnectFourBoard.vue
@@ -1,72 +1,68 @@
 <template>
     <div>
     <div id="board">
-        <img v-show="isHovered" ref="marker" class="marker animate__animated animate__bounce animate__infinite" :src="redMarker" alt="red marker" />
-
-        <div ref=board class="board-container d-none d-md-block" @mouseover="startCounterCheck" @mouseleave="stopCounterCheck">
+        <div ref=board class="board-container d-none d-md-block">
             <img class="back" :src="backBoardLarge" alt="back board" />
+            <div class="grid-container">
+                <div class="column" v-for="col in 7" :key="col" @mouseover="setMarkerPosition(col)" @mouseleave="removeMarkerPosition">
+                    <img v-if="markerColumn === col" class="marker animate__animated animate__bounce animate__infinite" :src="redMarker" alt="red marker" />
+                </div>
+                <div v-for="counter in playerCounters" :key="counter">
+                    <img ref="counter" class="red-counter" :src="largeRedCounter" alt="red counter">
+                </div>
+            </div>
             <img class="front" :src="frontBoardLarge" alt="front board" />
         </div>
         <div class="board-container d-block d-md-none">
             <img class="back" :src="backBoardSmall" alt="back board" />
+            <div class="grid-container">
+                <div v-for="counter in playerCounters" :key="counter">
+                    <img ref="counter" class="red-counter" :src="largeRedCounter" alt="red counter">
+                </div>
+                <img v-show="isHovered" ref="marker" class="marker animate__animated animate__bounce animate__infinite" :src="redMarker" alt="red marker" />
+            </div>
             <img class="front" :src="frontBoardSmall" alt="front board" />
         </div>
     </div>
 </div>
 </template>
 <script>
-import { ref, onMounted, onUnmounted, inject } from 'vue';
+import { ref, computed } from 'vue';
 import backBoardLarge from '../assets/images/board-layer-black-large.svg';
 import backBoardSmall from '../assets/images/board-layer-black-small.svg';
 import frontBoardLarge from '../assets/images/board-layer-white-large.svg';
 import frontBoardSmall from '../assets/images/board-layer-white-small.svg';
 import redMarker from '../assets/images/marker-red.svg';
+import largeRedCounter from '../assets/images/counter-red-large.svg';
+import smallRedCounter from '../assets/images/counter-red-small.svg';
 
 export default {
     setup() {
         const board = ref(null);
         const isHovered = ref(false);
         const marker = ref(null);
-        const columns = 7;
-        let columnPositions = [];
-        const forceNextTick = inject('forceNextTick');
-        const CENTER_OFFSET = 4;
+        const counter = ref(null);
+        const playerCounters = [];
+        const markerColumn = ref(null);
 
-        onMounted(() => {
-            forceNextTick(() => getColumnPositions());
 
-            window.addEventListener('resize', getColumnPositions);
-        });
+        function setMarkerPosition(col) {
+            markerColumn.value = col;
+        }
 
-        onUnmounted(() => {
-            window.removeEventListener('resize', getColumnPositions);
-        });
+        function removeMarkerPosition() {
+            markerColumn.value = null;
+        }
 
-        const getColumnPositions = () => {
-            columnPositions = [];
-            for (let i = 0; i < columns; i++) {
-                columnPositions.push(parseFloat(board.value.offsetLeft + (i * (board.value.offsetWidth / columns)) + (marker.value.width) - i * CENTER_OFFSET).toFixed(2));
+        const markerStyle = computed(() => {
+            if (markerColumn.value !== null) {
+                return {
+                    'grid-column': `${markerColumn.value}`,
+                    'grid-row': '0',
+                };
             }
-        }
-
-        const updateMarkerPosition = (event) => {
-            const x = event.clientX;
-            const validPositions = columnPositions.filter((c) => c < x);
-            const validPositionIndex = columnPositions.indexOf(validPositions[validPositions.length - 1]);
-            marker.value.style.left = `${columnPositions[validPositionIndex]}px`;
-        }
-
-        const startCounterCheck = () => {
-            marker.value.style.top = `${board.value.offsetTop - marker.value.height - 2}px`;
-            isHovered.value = true;
-
-            window.addEventListener('mousemove', updateMarkerPosition);
-        }
-
-        const stopCounterCheck = () => {
-            isHovered.value = false;
-            window.removeEventListener('mousemove', updateMarkerPosition);
-        }
+            return {};
+        });
 
         return {
             // images
@@ -75,15 +71,21 @@ export default {
             frontBoardLarge,
             frontBoardSmall,
             redMarker,
+            largeRedCounter,
+            smallRedCounter,
 
             // refs
             board,
             isHovered,
             marker,
+            playerCounters,
+            counter,
+            markerColumn,
 
             // methods
-            startCounterCheck,
-            stopCounterCheck,
+            setMarkerPosition,
+            removeMarkerPosition,
+            markerStyle,
         }
     }
 }
@@ -111,15 +113,38 @@ export default {
                 position: absolute;
                 top: 0;
                 left: 0;
-                z-index: 2;
+                z-index: 3;
+            }
+
+            .grid-container {
+                display: grid;
+                grid-template-columns: repeat(7, 1fr);
+                grid-template-rows: repeat(6, 1fr);
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
+            .column {
+                display: flex;
+                align-items: start;
+                justify-content: center;
+                grid-column: span 1;
+                grid-row: 1 / -1;
+                z-index: 10;
             }
         }
-        
+
         .marker {
-            position: absolute;
-            top: 0;
-            left: 0;
-            transform-origin: top left;
+            grid-row: 1;
+            z-index: 2;
+            margin: -2.5rem auto 0 auto;
+        }
+
+        .red-counter {
+            grid-column: span 1;
+            z-index: 2;
         }
     }
 </style>
